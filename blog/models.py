87e461b317b79f77e6d730e865c27d8abc58d4a6
maleka <mpho.maleka3@gmail.com>
2017-09-12 05:49:40 +0200

    trying to add image

--- blog/models.py
+++ blog/models.py
@@ -4,8 +4,9 @@ from django.core.validators import URLValidator
 class Post(models.Model):
     title = models.CharField(max_length = 140)
     body = models.TextField()
-    date = models.DateTimeField()
+    date = models.DateTimeField(blank=True, null=True)
     urladd = models.CharField(max_length = 140)
+    image = models.ImageField(blank=True, null=True)
 
     def __str__(self):
         return self.title
